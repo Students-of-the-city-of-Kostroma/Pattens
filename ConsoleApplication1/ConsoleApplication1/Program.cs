81b179c9d8b33eb3bd2d4d1b9718419c138ba62e
svyat935 <svyat935@mail.ru>
2019-11-01 14:16:19 +0300

    Change Rep.

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -5,33 +5,108 @@ using System.Text;
 
 namespace ConsoleApplication1
 {
+    public interface IStrategy
+    {
+        void DoIt(List<int[]> points, List<string[]> end);
+    }        
+
+    class StrategyAdd : IStrategy
+    {
+        Random rnd = new Random();
+        string[] temp = new string[2];
+
+        public StrategyAdd()
+        {            
+            Console.WriteLine("1.None\n" + 
+                              "2.Strelka\n" +
+                              "3.Romb\n");
+
+            for(int i = 0; i < 2; i++)
+            {
+                switch (Console.ReadLine())
+                {
+                    case "1": temp[i] = "None" ; break;
+                    case "2": temp[i] = "Strelka"; break;
+                    case "3": temp[i] = "Romb"; break;
+                    default: temp[i] = "None"; break;
+                }
+            }            
+        }
+
+        public void DoIt(List<int[]> points, List<string[]> end)
+        {
+            points.Add(new int[] { rnd.Next(-10, 10), rnd.Next(-10, 10), rnd.Next(-10, 10), rnd.Next(-10, 10)});
+            end.Add(temp);
+        }
+    }
+
+    class StrategyOutput : IStrategy
+    {
+        public void DoIt(List<int[]> points, List<string[]> end)
+        {
+            for(int i = 0; i < points.Count; i++)
+            {
+                Console.Write($"x1 = {points[i][0]}, y1 = {points[i][1]}, x2 = {points[i][2]}, y2 = {points[i][3]}");
+                Console.WriteLine($"[{end[i][0]}, {end[i][1]}]");
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    class StrategyDo
+    {
+        List<int[]> points = new List<int[]>();
+        List<string[]> end = new List<string[]>();
+
+        public StrategyDo()
+        {
+
+        }
+        public StrategyDo(IStrategy doit)
+        {
+            this.doit = doit;
+        }
+
+        public IStrategy doit { get; set; }
+
+        public void DoIt()
+        {
+            doit.DoIt(points, end);            
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            List<int[]> points = new List<int[]>();
-            string[] xy = { "X1=", "\tY1=", "\tX2=", "\tY2=" };
-            Random rnd = new Random();
+            StrategyDo strg = new StrategyDo();
             while (true)
             {
-                Console.WriteLine("1. add line (none, none)");
-                Console.WriteLine("2. add line (strelka, strelka)");
-                Console.WriteLine("3. add line (romb, romb)");
-                Console.WriteLine("4. print lines");
+                Console.Clear();
+                Console.WriteLine("1.Add in List");
+                Console.WriteLine("2.Output List");
+
                 switch (Console.ReadLine())
                 {
-                    case "1": points.Add(new int[]{
-                    rnd.Next(-10, 11),rnd.Next(-10, 11),
-                    rnd.Next(-10, 11),rnd.Next(-10, 11),
-                    1,1}); break;
-                    case "2": points.Add(new int[]{
-                    rnd.Next(-10, 11),rnd.Next(-10, 11),
-                    rnd.Next(-10, 11),rnd.Next(-10, 11),
-                    2,2}); break;
-                    case "3": points.Add(new int[]{
-                    rnd.Next(-10, 11),rnd.Next(-10, 11),
-                    rnd.Next(-10, 11),rnd.Next(-10, 11),
-                    3,3}); break;
+                    case "1":
+                        {
+                            strg.doit = new StrategyAdd();
+                            strg.DoIt();
+                            break;
+                        }
+                    case "2":
+                        {
+                            strg.doit = new StrategyOutput();
+                            strg.DoIt();
+                            Console.ReadLine();
+                            break;                            
+                        }
+                    default: break;
+                }
+            }
+            /*                            
                     case "4":
                         foreach (int[] point in points)
                         {
@@ -56,8 +131,7 @@ namespace ConsoleApplication1
                         Console.WriteLine("Неизвестная команда");
                         break;
                 }
-                Console.WriteLine();
-            }
+                    */
         }
     }
 }
